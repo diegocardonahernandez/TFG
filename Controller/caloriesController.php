a7a05b79a1ddf2fef8a9c5881a2130d1c06f7fe0
Diego Cardona <160233987+diegocardonahernandez@users.noreply.github.com>
2025-05-14 23:14:17 +0200

    commit

--- Controller/caloriesController.php
+++ Controller/caloriesController.php
@@ -4,7 +4,10 @@ require_once __DIR__ . '/../Functions/redirectView.php';
 require_once __DIR__ . '/../Model/Classes/Product.php';
 require_once __DIR__ . '/../Functions/recommendProducts.php';
 
+<<<<<<< HEAD
 $goal = 'gain'; // This should be set based on user input or session data
 $result = recommendProducts($goal);
+=======
+>>>>>>> 58f3a6295f4dabbbebdcf6151a7f21810b16ac3c
 
-renderLayout('caloriesCalc', ['recommendProducts' => $result]);
+renderLayout('caloriesCalc');
